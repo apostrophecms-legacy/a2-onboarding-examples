b32e19f01d6cbaac0e315df374cd746124c8a0d7
Alex Bea <abea@mailbox.org>
2021-08-13 13:23:24 -0500

    wip

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 // pm2-runtime is great at running node apps inside containers properly,
 // but its arguments can be mistaken for a command line Apostrophe task
 if (process.argv.find(arg => arg.match(/ProcessContainer/))) {
@@ -17,31 +15,13 @@ const apos = require('apostrophe')({
 
     // Apostrophe module configuration
 
-    // Note: most configuration occurs in the respective
-    // modules' directories. See lib/apostrophe-assets/index.js for an example.
-    // However any modules that are not present by default in Apostrophe must at
-    // least have a minimal configuration here: `moduleName: {}`
-
-    // If a template is not found somewhere else, serve it from the top-level
-    // `views/` folder of the project
-    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') },
-
     // Add an alt field to images schema, by default the title is used but
     // we recommend enabling the alt field for clarity.
     'apostrophe-images': {
       enableAltField: true,
     },
-
     'apostrophe-attachments': {
       svgImages: true
-    },
-
-    'social-links-widgets': {},
-
-    articles: {},
-    'articles-pages': {},
-    'articles-widgets': {},
-    'products': {},
-    'categories': {}
+    }
   }
 });
--- src/js/index.js
+++
@@ -1,3 +0,0 @@
-apos.utils.widgetPlayers['social-links'] = function(el, data, options) {
-  // Our Widget Client JS Here
-};
